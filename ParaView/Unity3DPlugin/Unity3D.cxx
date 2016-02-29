c3e34fa1a522ccc40ddc030c2519c5bd4d46d4a8
Rudolf Biczok <rcbiczok@gmail.com>
2016-02-29 09:01:55 +0100

    Add custom icons

--- ParaView/Unity3DPlugin/Unity3D.cxx
+++ ParaView/Unity3DPlugin/Unity3D.cxx
@@ -3,6 +3,7 @@
 #include <QApplication>
 #include <QStyle>
 #include <QDir>
+#include <QPixmap>
 #include <QMessageBox>
 #include <QProcess>
 
@@ -16,79 +17,102 @@
 #include "pqApplicationCore.h"
 #include "pqObjectBuilder.h"
 #include "pqServer.h"
-#include "pqServerManagerModel.h"
 #include "pqRenderView.h"
+
+#define UNITY_PLAYER_ACTION "UNITY_PLAYER_ACTION"
+
+#define UNITY_EDITOR_ACTION "UNITY_EDITOR_ACTION"
 
 //-----------------------------------------------------------------------------
 Unity3D::Unity3D(QObject* p) : QActionGroup(p)
 {
-	// let's use a Qt icon (we could make our own)
-	QIcon icon = qApp->style()->standardIcon(QStyle::SP_MessageBoxCritical);
-	QAction* a = new QAction(icon, "Create Sphere", this);
-	a->setData("SphereSource");
-	this->addAction(a);
+	// Player mode
+    QIcon embeddedActionIcon(QPixmap(":/Unity3D/resources/player.png"));
+    embeddedActionIcon.addPixmap(QPixmap(":/Unity3D/resources/player_selected.png"), QIcon::Mode::Selected);
+	QAction* embeddedAction = new QAction(embeddedActionIcon, "Show in Unity Player", this);
+	embeddedAction->setData(UNITY_PLAYER_ACTION);
+	this->addAction(embeddedAction);
 	QObject::connect(this, SIGNAL(triggered(QAction*)), this, SLOT(onAction(QAction*)));
+    
+    // Editor mode
+    QIcon exportActionIcon(QPixmap(":/Unity3D/resources/editor.png"));
+    exportActionIcon.addPixmap(QPixmap(":/Unity3D/resources/editor_selected.png"), QIcon::Mode::Selected);
+    QAction* exportAction = new QAction(exportActionIcon, "Export to Unity Editor", this);
+    exportAction->setData(UNITY_EDITOR_ACTION);
+    this->addAction(exportAction);
+    QObject::connect(this, SIGNAL(triggered(QAction*)), this, SLOT(onAction(QAction*)));
 }
 
 //-----------------------------------------------------------------------------
-void Unity3D::onAction(QAction* a)
-{
+void Unity3D::onAction(QAction* a) {
 	pqApplicationCore* core = pqApplicationCore::instance();
-	//pqObjectBuilder* builder = core->getObjectBuilder();
 	pqServerManagerModel* sm = core->getServerManagerModel();
 
-	// get all the pqRenderView instances.
-	QList<pqRenderView*> renderViews = sm->findItems<pqRenderView*>();
-
 	/// Check that we are connect to some server (either builtin or remote).
-	if (sm->getNumberOfItems<pqServer*>())
-	{
-		// just create it on the first server connection
-		//pqServer* s = sm->getItemAtIndex<pqServer*>(0);
-
-        QProcess* process = new QProcess(this);
-        QString file = "/Users/rcbiczok/Bachelorarbeit/ParaUnity/Prototype/Unity/ParaViewEmbeddedPlayer/build.app";
-        process->start(file);
-        
-        QMessageBox dialog(QMessageBox::Warning, "Debug",
-                           process->errorString());
-        dialog.exec();
-        
-		/*vtkSMRenderViewProxy* renderProxy = renderViews[0]->getRenderViewProxy();
-
-		QString exportLocations(QDir::tempPath() + "/Unity3DPlugin");
-
-		QStringList activeUnityInstances;
-		foreach(const QString &dir, QDir(exportLocations).entryList()) {
-			if (dir != "." && dir != "..") {
-				QString lockFile = exportLocations + "/" + dir + "/lock";
-				if (QFile::exists(lockFile) && !QFile::remove(lockFile)) {
-					activeUnityInstances << dir;
-				}
-			}
-		}
-
-		if (activeUnityInstances.isEmpty()) {
-			QMessageBox dialog(QMessageBox::Warning, "Unity not running",
-				"No suitable instance of the Unity Editor is running!");
-			dialog.setText("Start a prepared Unity project first");
-			dialog.exec();
-		}
-		else if (activeUnityInstances.length() == 1) {
-			vtkSmartPointer<vtkX3DExporter> exporter =
-				vtkSmartPointer<vtkX3DExporter>::New();
-			QString exportFile = exportLocations + "/" + activeUnityInstances[0] + "/paraview_output.tmp";
-			exporter->SetInput(renderProxy->GetRenderWindow());
-			exporter->SetFileName(exportFile.toLatin1());
-			exporter->Write();
-			QFile(exportFile).rename(exportLocations + "/" + activeUnityInstances[0] + "/paraview_output.x3d");
-		}
-		else {
-			QMessageBox dialog(QMessageBox::Critical, "Error",
-				"Multiple Unity instances are running");
-			dialog.exec();
-		}*/
+	if (sm->getNumberOfItems<pqServer*>()) {
+     
+        if (a->data() == UNITY_PLAYER_ACTION) {
+            this->showInUnityPlayer(sm);
+        } else if(a->data() == UNITY_EDITOR_ACTION) {
+            this->exportToUnityEditor(sm);
+        } else {
+            throw std::runtime_error("Unexpe action type\n");
+        }
 	}
 }
 
+//-----------------------------------------------------------------------------
+void Unity3D::showInUnityPlayer(pqServerManagerModel* sm) {
+    QProcess* process = new QProcess(this);
+    QString file = "/Users/rcbiczok/Bachelorarbeit/ParaUnity/Prototype/Unity/ParaViewEmbeddedPlayer/build/build.app";
+    process->start(file);
+    
+    QMessageBox dialog(QMessageBox::Warning, "Debug",
+                       process->errorString());
+    dialog.exec();
+}
+
+//-----------------------------------------------------------------------------
+void Unity3D::exportToUnityEditor(pqServerManagerModel* sm) {
+    // get all the pqRenderView instances.
+    QList<pqRenderView*> renderViews = sm->findItems<pqRenderView*>();
+    
+    // just create it on the first server connection
+    //pqServer* s = sm->getItemAtIndex<pqServer*>(0);
+    
+    vtkSMRenderViewProxy* renderProxy = renderViews[0]->getRenderViewProxy();
+    
+    QString exportLocations(QDir::tempPath() + "/Unity3DPlugin");
+    
+    QStringList activeUnityInstances;
+    foreach(const QString &dir, QDir(exportLocations).entryList()) {
+        if (dir != "." && dir != "..") {
+            QString lockFile = exportLocations + "/" + dir + "/lock";
+            if (QFile::exists(lockFile) && !QFile::remove(lockFile)) {
+                activeUnityInstances << dir;
+            }
+        }
+    }
+     
+    if (activeUnityInstances.isEmpty()) {
+        QMessageBox dialog(QMessageBox::Warning, "Unity not running",
+                           "No suitable instance of the Unity Editor is running!");
+        dialog.setText("Start a prepared Unity project first");
+        dialog.exec();
+    }
+    else if (activeUnityInstances.length() == 1) {
+        vtkSmartPointer<vtkX3DExporter> exporter =
+        vtkSmartPointer<vtkX3DExporter>::New();
+        QString exportFile = exportLocations + "/" + activeUnityInstances[0] + "/paraview_output.tmp";
+        exporter->SetInput(renderProxy->GetRenderWindow());
+        exporter->SetFileName(exportFile.toLatin1());
+        exporter->Write();
+        QFile(exportFile).rename(exportLocations + "/" + activeUnityInstances[0] + "/paraview_output.x3d");
+    }
+    else {
+        QMessageBox dialog(QMessageBox::Critical, "Error",
+                           "Multiple Unity instances are running");
+        dialog.exec();
+    }
+}
 
--- ParaView/Unity3DPlugin/Unity3D.h
+++ ParaView/Unity3DPlugin/Unity3D.h
@@ -2,16 +2,18 @@
 #define Unity3D_h
 
 #include <QActionGroup>
+#include "pqServerManagerModel.h"
 
 class Unity3D : public QActionGroup
 {
-  Q_OBJECT
+    Q_OBJECT
 public:
-  Unity3D(QObject* p);
-
+    Unity3D(QObject* p);
+private:
+    void showInUnityPlayer(pqServerManagerModel* sm);
+    void exportToUnityEditor(pqServerManagerModel* sm);
 public slots:
-  /// Callback for each action triggerred.
-  void onAction(QAction* a);
+    void onAction(QAction* a);
 };
 
 #endif
