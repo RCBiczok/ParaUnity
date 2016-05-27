ea91b1229bbecb99d23de488cca707764449086f
Rudolf Biczok <rcbiczok@gmail.com>
2016-05-28 01:01:58 +0200

    Add InteliJ project files

---
+++ ParaView/Unity3DPlugin/LoadingSplashScreen.h
@@ -0,0 +1,32 @@
+#ifndef LoadingSplashScreen_h
+#define LoadingSplashScreen_h
+
+#include <QtWidgets>
+#include <QString>
+
+class LoadingSplashScreen : public QSplashScreen {
+  Q_OBJECT
+public:
+  LoadingSplashScreen(const QString &fileName) : movie(fileName) {
+    setPixmap(QPixmap::fromImage(QImage(fileName)));
+    movie.start();
+    connect(&movie, SIGNAL(updated(QRect)), this, SLOT(frameUpdate()));
+  }
+
+  ~LoadingSplashScreen() {}
+
+  void paintEvent(QPaintEvent *event) {
+    QPainter painter(this);
+    painter.drawPixmap(movie.frameRect(), movie.currentPixmap());
+  }
+
+private slots:
+  void frameUpdate() {
+    setPixmap(movie.currentPixmap());
+    update();
+  }
+
+private:
+  QMovie movie;
+};
+#endif // LoadingSplashScreen_h
